7aa114d8e8d8d3a2d0cfdd4b7c9245b5f5128e6c
Packy Anderson <panderson@advancedinfomanagement.com>
2021-04-20 15:41:40 -0400

    Allow for systems without a perl executable

--- bashrc.d/enabled/00_path_functions.sh
+++ bashrc.d/enabled/00_path_functions.sh
@@ -1,43 +1,65 @@
 #!/usr/bin/env bash
 # for syntax highlightling in emacs, mostly
 
-unshift_path () {
-  export PATH=$(perl -e '
-    my $sep  = q{:};
-    my $alt  = join q{|}, @ARGV;
-    my $add  = qr/^(?:$alt)$/;
-    # get the dirs in $PATH that are not being put on the front
-    my @PATH = grep { !/$add/ } split /$sep/, $ENV{PATH};
-    print join $sep, @ARGV, @PATH;
-  ' "$@")
-}
+if type perl >/dev/null 2>&1; then
 
-push_path () {
-  export PATH=$(perl -e '
-    my $sep  = q{:};
-    my $alt  = join q{|}, @ARGV;
-    my $add  = qr/^(?:$alt)$/;
-    # get the dirs in $PATH that are not being put on the end
-    my @PATH = grep { !/$add/ } split /$sep/, $ENV{PATH};
-    print join $sep, @PATH, @ARGV;
-  ' "$@")
-}
+  unshift_path () {
+    export PATH=$(perl -e '
+      my $sep  = q{:};
+      my $alt  = join q{|}, @ARGV;
+      my $add  = qr/^(?:$alt)$/;
+      # get the dirs in $PATH that are not being put on the front
+      my @PATH = grep { !/$add/ } split /$sep/, $ENV{PATH};
+      print join $sep, @ARGV, @PATH;
+    ' "$@")
+  }
 
-uniq_path () {
-  export PATH=$(perl -e '
-    my $sep  = q{:};
-    my %seen;
-    my @PATH = grep { ! $seen{$_}++ } split /$sep/, $ENV{PATH};
-    print join $sep, @PATH, @ARGV;
-  ' "$@")
-}
+  push_path () {
+    export PATH=$(perl -e '
+      my $sep  = q{:};
+      my $alt  = join q{|}, @ARGV;
+      my $add  = qr/^(?:$alt)$/;
+      # get the dirs in $PATH that are not being put on the end
+      my @PATH = grep { !/$add/ } split /$sep/, $ENV{PATH};
+      print join $sep, @PATH, @ARGV;
+    ' "$@")
+  }
 
-explode_path () {
-  perl -e '
-    my $PATH  = $ENV{PATH};
-    my $sep   = q{:};
-    my @parts = split /$sep/, $PATH;
-    print "PATH:\n";
-    print "  ", join("\n  ", @parts), "\n";
-  '
-}
+  uniq_path () {
+    export PATH=$(perl -e '
+      my $sep  = q{:};
+      my %seen;
+      my @PATH = grep { ! $seen{$_}++ } split /$sep/, $ENV{PATH};
+      print join $sep, @PATH, @ARGV;
+    ' "$@")
+  }
+
+  explode_path () {
+    perl -e '
+      my $PATH  = $ENV{PATH};
+      my $sep   = q{:};
+      my @parts = split /$sep/, $PATH;
+      print "PATH:\n";
+      print "  ", join("\n  ", @parts), "\n";
+    '
+  }
+
+else
+  # there isn't a perl in this environment!
+
+  unshift_path () {
+    echo >/dev/null;
+  }
+
+  push_path () {
+    export PATH=$PATH:$1
+  }
+
+  uniq_path () {
+    echo >/dev/null;
+  }
+
+  explode_path () {
+    echo $PATH
+  }
+fi
